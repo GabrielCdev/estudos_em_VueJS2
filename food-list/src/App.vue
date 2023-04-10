e1aeb8a0e4dd6f04c23b5ea79e7030b25efe718d
gabrielcalheiros <gabrielcalheiros@uneb.br>
2023-04-10 10:45:58 -0300

    Food list with Vue 2

---
+++ food-list/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Minha lista</h1>
+    <input @keyup.enter="addFood" type="text" v-model="currentFood">
+    <button @click="addFood">Add!</button>
+    <food-list @stop="handleStop" :foods="foods" />
+  </div>
+</template>
+
+<script>
+import FoodList from './components/FoodList'
+
+export default {
+  components: { FoodList },
+  data: () => ({
+    foods: ['Maçã', 'Pera'],
+    currentFood: ''
+  }),
+  methods: {
+    handleStop(payload) {
+      console.log('payload =>', payload)
+      window.alert(payload)
+    },
+    addFood() {
+      this.foods.push(this.currentFood)
+      this.currentFood = ''
+    }
+  }
+}
+</script>
---
+++ food-list/src/components/FoodList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="(food, index) in foods" :key="`${food}-${index}`">{{ food }}</li>
+  </ul>
+</template>
+
+<script>
+const MAX_FOODS = 5
+
+export default {
+  props: {
+    foods: {
+      type: Array, default: () => []
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  watch: {
+    foods() {
+      if (this.foods.length === MAX_FOODS) {
+        this.$emit('stop', 'Está cheio!!!') // Emite um evento de STOP + aviso
+      }
+    }
+  }
+}
+</script>
